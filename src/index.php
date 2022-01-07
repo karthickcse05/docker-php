9165c51bbbaa4630dbf1a8e1be62328c4342fc7f
Karthick.S <karthick.s@tranetechnologies.com>
2022-01-07 15:55:26 +0530

    Testing GHA Actions

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Docker! <br>";
+echo "Hello, World from Docker-PHP! <br>";
 echo "Hello AWS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
