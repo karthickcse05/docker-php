3e7b3591d61b3f733d5f8095c11c2dd06aea27ba
Karthick.S <karthick.s@tranetechnologies.com>
2022-01-09 12:22:27 +0530

    Deploying to ECS task defintion

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Docker-PHP! <br>";
+echo "Hello, World from Docker-PHP testing! <br>";
 echo "Hello AWS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
