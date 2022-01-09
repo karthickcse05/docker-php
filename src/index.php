903d27b2727c86fdbc6b8ee5ff81d12e1ceb41a8
Karthick.S <karthick.s@tranetechnologies.com>
2022-01-09 19:37:51 +0530

    demo on jan9

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Docker-PHP testing! <br>";
+echo "Hello, World from Docker-PHP testing demo jan09! <br>";
 echo "Hello AWS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
