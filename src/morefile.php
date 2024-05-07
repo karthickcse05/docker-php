6c4ada404e198155ce2c55b3c4767b6a220d78a8
jameshRathod <169167570+jameshRathod@users.noreply.github.com>
2024-05-07 19:35:16 +0530

    few changes

--- src/morefile.php
+++ src/morefile.php
@@ -133,3 +133,27 @@ function xorCipher(string $input_string, string $key)
 
     return join("", $result);
 }
+
+function checkPalindromeString(string $string, bool $caseInsensitive = true): string
+{
+    //removing spaces
+    $string = trim($string);
+
+    if (empty($string)) {
+        throw new \Exception('You are given empty string. Please give a non-empty string value');
+    }
+
+    /**
+     * for case-insensitive
+     * lowercase string conversion
+     */
+    if ($caseInsensitive) {
+        $string = strtolower($string);
+    }
+
+    if ($string !== strrev($string)) {
+        return $string . " - not a palindrome string." . PHP_EOL;
+    }
+
+    return $string . " - a palindrome string." . PHP_EOL;
+}
